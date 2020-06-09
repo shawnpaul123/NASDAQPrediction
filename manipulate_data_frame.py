57ff369629f29d4ee668b7f35f113e72b3275384
shawnpaul123 <31085457+shawnpaul123@users.noreply.github.com>
2020-06-09 08:57:48 -0400

    ediTED FILE WITH CLASSES

--- manipulate_data_frame.py
+++ manipulate_data_frame.py
@@ -9,17 +9,25 @@ import logging
 from numba import jit
 from sklearn import preprocessing
 
-  
-def create_indicators(df1,price_col,syms):
+ class Stock_Data_Test_Args():
 
+ 	def _init_():
+ 	    #stock data
+ 		self.stocks = #
+ 		self.tickers = #
+ 		self.NASDAQ_URL = r"https://en.wikipedia.org/wiki/NASDAQ-100"
+
+
+def create_indicators(df1,price_col,syms):
     df = df1
     for sym in syms:
     
         df['ma7_'.format(sym)] = df[price_col.format(sym)].rolling(window=7).mean()
         df['ma14_'.format(sym)] = df[price_col.format(sym)].rolling(window=14).mean()
         df['ema_'.format(sym)] = df[price_col.format(sym)].rolling(window=7).mean()
- 
-    return df
+     return df
 
 
-
+#delte after use
+def get_test_stock():
+	pass
